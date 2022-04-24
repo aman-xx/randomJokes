3ca0539ab78078988b24d44c9941827237219960
Aman Kumar <amankumarmznup@gmail.com>
2022-04-25 01:03:57 +0530

    my fault fix

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+{
+  /* <div id="joke" class="joke">Awesome joke is loading😳...</div>
+<button id="jokeBtn" class="btn">Next Joke</button> */
+}
+
 const jokes = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
 
-const generateJokes = async () => {
-  try {
-    const setHeader = {
-      headers: {
-        Accept: "application/json",
-      },
-    };
-    const res = await fetch("https://icanhazdadjoke.com", setHeader);
-    const data = await res.json();
-    jokes.innerHTML = data.joke;
-  } catch (err) {
+const generateJokes = async() => {
+    try {  const setHeader = {
+    headers: {
+      Accept: "application/json"
+    }
+  };  const res= await fetch('https://icanhazdadjoke.com', setHeader);
+  const data =await res.json();
+  jokes.innerHTML=data.joke;
+}catch(err){
     console.log(`The error is ${err}`);
-  }
-};
+}
+ 
+}
 
 jokeBtn.addEventListener("click", generateJokes);
 generateJokes();
